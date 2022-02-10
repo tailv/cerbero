77ef0091a16b92c7a2b96ab39cb39d5176c6db03
tailv <vantai.le@live.com>
2022-02-10 14:42:25 +0700

    Update

--- cerbero/bootstrap/android.py
+++ cerbero/bootstrap/android.py
@@ -27,7 +27,7 @@ from cerbero.utils import _, shell
 NDK_VERSION = 'r23b'
 NDK_BASE_URL = 'https://dl.google.com/android/repository/android-ndk-%s-%s.zip'
 NDK_CHECKSUMS = {
-    'android-ndk-r23b-linux.zip': 'c6e97f9c8cfe5b7be0a9e6c15af8e7a179475b7ded23e2d1c1fa0945d6fb4382',
+    'android-ndk-r23b-darwin.zip': 'f8672ea777521382da3f5e2dc57d8658',
 }
 
 class AndroidBootstrapper (BootstrapperBase):
